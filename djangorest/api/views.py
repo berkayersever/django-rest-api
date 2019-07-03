ffe033b93e6b844c4d93d41336bebd3d1012fd13
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-07-03 16:32:28 +0200

    Update views.py

--- djangorest/api/views.py
+++ djangorest/api/views.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.models import User
 from rest_framework import generics, permissions
 from .permissions import IsOwner
-from .serializers import BucketlistSerializer
+from .serializers import BucketlistSerializer, UserSerializer
 from .models import Bucketlist
 
 
@@ -21,3 +22,15 @@ class DetailsView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Bucketlist.objects.all()
     serializer_class = BucketlistSerializer
     permission_classes = (permissions.IsAuthenticated, IsOwner)
+
+
+class UserView(generics.ListAPIView):
+    """View to list the user queryset."""
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetailsView(generics.RetrieveAPIView):
+    """View to retrieve a user instance."""
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
