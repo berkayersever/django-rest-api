bbbae0927930735c68294baf826b196fc4d19e38
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-07-03 14:54:03 +0200

    Update tests.py

--- djangorest/api/tests.py
+++ djangorest/api/tests.py
@@ -24,46 +24,47 @@ class ModelTestCase(TestCase):
         self.assertNotEqual(old_count, new_count)
 
 
-class ViewTestCase(TestCase):
+class ViewsTestCase(TestCase):
     """Test suite for the api views."""
 
     def setUp(self):
         """Define the test client and other test variables."""
+        user = User.objects.create(username="nerd")
+
+        # Initialize client and force it to use authentication
         self.client = APIClient()
-        self.bucketlist_data = {'name': 'Go to Ibiza'}
-        self.response = self.client.post(
-            reverse('create'),
-            self.bucketlist_data,
-            format="json")
+        self.client.force_authenticate(user=user)
+
+        # Since user model instance is not serializable, use its Id/PK
+        self.bucketlist_data = {'name': 'Go to Ibiza', 'owner': user.id}
+        self.response = self.client.post(reverse('create'), self.bucketlist_data, format="json")
 
     def test_api_can_create_a_bucketlist(self):
         """Test the api has bucket creation capability."""
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
 
+    def test_authorization_is_enforced(self):
+        """Test that the api has user authorization."""
+        new_client = APIClient()
+        res = new_client.get('/bucketlists/', kwargs={'pk': 3}, format="json")
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
     def test_api_can_get_a_bucketlist(self):
         """Test the api can get a given bucketlist."""
-        bucketlist = Bucketlist.objects.get()
-        response = self.client.get(
-            reverse('details',
-                    kwargs={'pk': bucketlist.id}), format="json")
-
+        bucketlist = Bucketlist.objects.get(id=1)
+        response = self.client.get('/bucketlists/', kwargs={'pk': bucketlist.id}, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertContains(response, bucketlist)
 
     def test_api_can_update_bucketlist(self):
         """Test the api can update a given bucketlist."""
+        bucketlist = Bucketlist.objects.get()
         change_bucketlist = {'name': 'Something new'}
-        res = self.client.put(
-            reverse('details', kwargs={'pk': bucketlist.id}),
-            change_bucketlist, format='json'
-        )
+        res = self.client.put(reverse('details', kwargs={'pk': bucketlist.id}), change_bucketlist, format='json')
         self.assertEqual(res.status_code, status.HTTP_200_OK)
 
     def test_api_can_delete_bucketlist(self):
         """Test the api can delete a bucketlist."""
         bucketlist = Bucketlist.objects.get()
-        response = self.client.delete(
-            reverse('details', kwargs={'pk': bucketlist.id}),
-            format='json',
-            follow=True)
+        response = self.client.delete(reverse('details', kwargs={'pk': bucketlist.id}), format='json', follow=True)
         self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
