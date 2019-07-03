dd0b757c34dc6ffae9ca95362e5f8ad7d91b0115
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-07-03 16:33:46 +0200

    Update serializers.py

--- djangorest/api/serializers.py
+++ djangorest/api/serializers.py
@@ -12,3 +12,14 @@ class BucketlistSerializer(serializers.ModelSerializer):
         model = Bucketlist
         fields = ('id', 'name', 'owner', 'date_created', 'date_modified')
         read_only_fields = ('date_created', 'date_modified')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """A user serializer to aid in authentication and authorization."""
+    bucketlists = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Bucketlist.objects.all())
+
+    class Meta:
+        """Map this serializer to the default django user model."""
+        model = User
+        fields = ('id', 'username', 'bucketlists')
